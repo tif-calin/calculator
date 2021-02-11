e1bd3d107c24c01ae3b6cad3275303ae788a4245
Calin Tif <culitif@tuta.io>
2021-02-10 16:43:52 -0800

    made basic calc

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
 // import functions and grab DOM elements
+const inNum1 = document.getElementById('calc-in1');
+const inNum2 = document.getElementById('calc-in2');
+const inFnc = document.getElementById('calc-select-fnc');
+
+const outResult = document.getElementById('calc-output');
+
+const btn = document.getElementById('calc-button');
+
 
 // initialize state
 
 // set event listeners to update state and DOM
+
+btn.addEventListener('click', () => {
+    let result;
+    switch (inFnc.value) {
+        case "-":
+            result = Number(inNum1.value) - Number(inNum2.value);
+            break;
+        case "*":
+            result = (Number(inNum1.value) * Number(inNum2.value)).toFixed(3);
+            break;
+        case "/":
+            result = (Number(inNum1.value) / Number(inNum2.value)).toFixed(3);
+            break;
+        default:
+            result = Number(inNum1.value) + Number(inNum2.value);
+    }
+
+    outResult.innerText = result;
+})
